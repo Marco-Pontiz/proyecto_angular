222c73903c8dc8f6a4f3e8c7fd12a3ba42eed535
Marco Pontiz <marcopontiz@gmail.com>
2023-07-24 04:52:31 -0300

    Arreglando vista previa y añadiendo unas validaciones

--- src/app/dashboard/pages/users/users.component.ts
+++ src/app/dashboard/pages/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent {
+  nameControl = new FormControl(null, [
+    Validators.required, 
+    Validators.minLength(2)
+  ]);
+  surnameControl = new FormControl();
+  emailControl = new FormControl();
+  passwordControl = new FormControl();
+  
+  userForm = new FormGroup({
+    name: this.nameControl,
+    surname: this.surnameControl,
+    email: this.emailControl,
+    password: this.passwordControl
+  });
 
+  onSubmit(): void {
+    alert(JSON.stringify(this.userForm.value));
+  }
 }
